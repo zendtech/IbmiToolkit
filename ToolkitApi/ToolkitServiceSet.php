e243e5e7ff8d6c017e35c5836aced52ed9ea16d3
Chuk Shirley <chukShirley@gmail.com>
2018-02-19 06:48:06 -0600

    Use ZS9 library in default settings

--- ToolkitApi/ToolkitServiceSet.php
+++ ToolkitApi/ToolkitServiceSet.php
@@ -2,9 +2,9 @@
 
 // default settings
 
-define('XMLSERVICELIB', 'ZENDSVR6');   //library with IBM XMLTOOLKIT objects
+define('XMLSERVICELIB', 'ZENDPHP7');   //library with IBM XMLTOOLKIT objects
 define('DFTLIB', 'QGPL');              //library for creation of temporary objects
-define('ZSTOOLKITLIB', 'ZENDSVR6');    //library with service program
+define('ZSTOOLKITLIB', 'ZENDPHP7');    //library with service program
 define('ZSTOOLKITPGM', 'ZSXMLSRV');    //service program name
 define('DBPROTOCOL', 'ibm_db2');
 define('XMLINTERNALKEY', '/tmp/Toolkit'); // default IPC (InternalKey)
