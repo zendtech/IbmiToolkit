91537a9512837bb0be3fcbcd1e940767d5a28774
Chuk Shirley <chukShirley@gmail.com>
2019-12-06 07:42:13 -0600

    Add functional tests for odbc and pdo transports

--- tests/functional/ToolkitTest.php
+++ tests/functional/ToolkitTest.php
@@ -5,85 +5,82 @@ declare(strict_types=1);
 namespace ToolkitFunctionalTest;
 
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
 use ToolkitApi\Toolkit;
 
 final class ToolkitTest extends TestCase
 {
     const TEMPORARY_LIBRARY = 'MYLIB1';
 
+    /**
+     * @var array
+     */
     private $connectionOptions;
+
+    /**
+     * @var array
+     */
+    private $toolkitOptions;
 
     public function setUp(): void
     {
-        $this->connectionOptions = [
-            'dsn' => '*LOCAL',
-            'user' => '*CURRENT',
-            'password' => '',
-        ];
+        $config = getConfig();
 
-        $rpgSource = dirname(__DIR__ . '../') . DIRECTORY_SEPARATOR . 'ADDONE.RPGLE';
-        $this->runShellCommand("system \"CRTLIB LIB(" . self::TEMPORARY_LIBRARY . ")");
-        $this->runShellCommand("system \"CRTBNDRPG PGM(" . self::TEMPORARY_LIBRARY . "/ADDONE) SRCSTMF('$rpgSource')\"");
+        $this->connectionOptions = $config['db']['odbc'];
+        $this->toolkitOptions = $config['toolkit'];
     }
 
-    public function tearDown(): void
+    /**
+     * @throws \Exception
+     */
+    public function testCanPassPdoOdbcObjectToToolkit()
     {
-        $this->runShellCommand("system \"DLTLIB LIB(" . self::TEMPORARY_LIBRARY . ")");
+        $pdo = new \PDO(
+            'odbc:' . $this->connectionOptions['dsn'],
+            $this->connectionOptions['username'],
+            $this->connectionOptions['password'],
+            [
+                'platform' => $this->connectionOptions['platform'],
+                'platform_options' => [
+                    'quote_identifiers' => $this->connectionOptions['platform_options']['quote_identifiers'],
+                ]
+            ]
+        );
+
+        $toolkit = new Toolkit($pdo, null, null, 'pdo');
+        $toolkit->setOptions($this->toolkitOptions);
+
+        $this->assertInstanceOf(Toolkit::class, $toolkit);
     }
 
-    private function runShellCommand(string $command): string
+    /**
+     * @throws \Exception
+     */
+    public function testCanPassOdbcResourceToToolkit()
     {
-        $process = Process::fromShellCommandline($command);
-        $process->setTimeout(0);
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        return $process->getOutput();
-    }
-
-    public function testCanCallRpgProgramByPassingOdbcResourceObjectToToolkit()
-    {
-        $connection = odbc_connect($this->connectionOptions['dsn'], $this->connectionOptions['user'], $this->connectionOptions['password']);
+        $connection = odbc_connect($this->connectionOptions['dsn'], $this->connectionOptions['username'], $this->connectionOptions['password']);
 
         if (!$connection) {
             throw new \Exception('Connection failed');
         }
         $toolkit = new Toolkit($connection, null, null, 'odbc');
+        $toolkit->setOptions($this->toolkitOptions);
 
-        $param[] = $toolkit::AddParameterPackDec('both', 10, 0, 'NUMBER', 'NUMBER', 0);
-        $result = $toolkit->pgmCall("ADDONE", self::TEMPORARY_LIBRARY, $param, null, null);
-        if (!$result) {
-            throw new \Exception('Program call failed');
-        }
-        $out = $result['io_param'];
-
-        $loadId = 0;
-        foreach ($out as $key => $value) {
-            $loadId = $value;
-        }
-        $this->assertTrue(strlen($loadId) > 0);
+        $this->assertInstanceOf(Toolkit::class, $toolkit);
     }
 
-//    public function testCanCallRpgProgramByPassingOdbcConnectionParametersToToolkit()
-//    {
-//        $toolkit = new Toolkit($this->connectionOptions['dsn'], $this->connectionOptions['user'], $this->connectionOptions['password'], 'odbc');
-//
-//        $param[] = $toolkit::AddParameterPackDec('both', 10, 0, 'NUMBER', 'NUMBER', 0);
-//        $result = $toolkit->pgmCall("ADDONE", self::TEMPORARY_LIBRARY, $param, null, null);
-//        if (!$result) {
-//            throw new \Exception('Program call failed');
-//        }
-//        $out = $result['io_param'];
-//
-//        $loadId = 0;
-//        foreach ($out as $key => $value) {
-//            $loadId = $value;
-//        }
-//        $this->assertTrue(strlen($loadId) > 0);
-//    }
+    /**
+     * @throws \Exception
+     */
+    public function testCanPassOdbcConnectionParametersToToolkit()
+    {
+        $toolkit = new Toolkit(
+            $this->connectionOptions['dsn'],
+            $this->connectionOptions['username'],
+            $this->connectionOptions['password'],
+            'odbc'
+        );
+        $toolkit->setOptions($this->toolkitOptions);
 
+        $this->assertInstanceOf(Toolkit::class, $toolkit);
+    }
 }
