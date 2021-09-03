d567d0d22ffd71616cb157282d58076b92193b74
Alan Seiden <alan@alanseiden.com>
2021-09-03 14:53:28 -0400

    Add a basic $ds array

--- samples/data-structure-array-with-counter.php
+++ samples/data-structure-array-with-counter.php
@@ -12,6 +12,12 @@ This code snippet shows how to set up your parameters to allow an RPG program to
 // Say you have set up data structure params in the $ds variable.
 // Add that data structure and the counter field to the parameter array.
 
+$ds = [];
+$ds[] = $conn->AddParameterChar('in', 21,'Part', 'PTPRT', 'A123');
+$ds[] = $conn->AddParameterChar('in', 3,'Vendor', 'PTVEN', '825');
+$ds[] = $conn->AddParameterChar('out', 20,'Description', 'PTDES', $out);
+$ds[] = $conn->AddParameterZoned('out', 9, 2, 'Price', 'PTPRC', $out);
+
 // Multi-occurrence data structure with maximum dimension set to 10000 but whose final output count will be determined by MYCOUNTER. Name the counter whatever you wish.
 $param[] = $conn->AddDataStruct($ds, 'MULTDS')
                               ->setParamDimension(10000) // max of 10000 records
